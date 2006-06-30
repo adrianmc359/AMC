f4c02f13242811e557f0547c3328b0ef2228c8d4
adrian <adrian@651d49b6-7517-0410-9a13-a4ecc4006b63>
2006-06-30 19:42:56 +0000

    A View of XY Graph.
    git-svn-id: file:///home/adrian/Documents/SVNRepository/Java/AMC@8 651d49b6-7517-0410-9a13-a4ecc4006b63

---
+++ trunk/AMC/src/org/amc/swing/XY_GraphPane.java
@@ -0,0 +1,40 @@
+package org.amc.swing;
+
+import javax.swing.*;
+import java.awt.*;
+import org.amc.swing.graph.*;
+/**
+ * <p>Project: AMC </p>
+ * <p>file: $URL$<p>
+ * <p>Created on Jun 30, 2006</p>
+ * A View of XY graph
+ * @author adrian
+ * @version $Revision$
+ */
+
+public class XY_GraphPane extends JPanel
+{
+	XY_Graph graph;
+	/**
+	 * 
+	 * Constructor for XY_GraphPane.java
+	 */
+	public XY_GraphPane()
+	{
+		super();
+		try
+		{
+			jbInit();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	private void jbInit() throws Exception
+	{
+		graph=new XY_Graph();
+	}
+
+}
