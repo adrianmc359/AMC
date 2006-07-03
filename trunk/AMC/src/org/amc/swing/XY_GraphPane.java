899ae6c3af6f3ee34eb0fc25a1d42aeac3f2657d
adrian <adrian@651d49b6-7517-0410-9a13-a4ecc4006b63>
2006-07-03 09:27:25 +0000

    Moved X_Axis and Y_Axis out of class. Moved getDivisions method to super class. fixed bug with displaying graph on startup.
    git-svn-id: file:///home/adrian/Documents/SVNRepository/Java/AMC@19 651d49b6-7517-0410-9a13-a4ecc4006b63

--- trunk/AMC/src/org/amc/swing/XY_GraphPane.java
+++ trunk/AMC/src/org/amc/swing/XY_GraphPane.java
@@ -6,14 +6,11 @@ import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-
+import java.awt.geom.*;
 import org.amc.swing.graph.*;
 import org.amc.util.Utils;
 
-import com.sun.org.apache.bcel.internal.verifier.structurals.Frame;
+
 /**
  * <p>Project: AMC </p>
  * <p>file: $URL$<p>
@@ -57,6 +54,7 @@ public class XY_GraphPane extends JPanel implements Graph
 	}
 	private void jbInit() throws Exception
 	{
+		
 		graph=new XY_Graph();
 		this.setLayout(new BorderLayout());
 		
@@ -68,9 +66,9 @@ public class XY_GraphPane extends JPanel implements Graph
 		y_axis_label=new JLabel(graph.getY_axis());
 		y_axis_label.setHorizontalAlignment(SwingConstants.CENTER);
 		this.add(canvas,BorderLayout.CENTER);
-		this.add(title,BorderLayout.NORTH);
-		this.add(x_axis_label,BorderLayout.SOUTH);
-		this.add(y_axis_label,BorderLayout.WEST);
+		//this.add(title,BorderLayout.NORTH);
+		//this.add(x_axis_label,BorderLayout.SOUTH);
+		//this.add(y_axis_label,BorderLayout.WEST);
 		
 	}
 	
@@ -124,17 +122,7 @@ public class XY_GraphPane extends JPanel implements Graph
 	{
 		return graph.noOfDataSets();
 	}
-	protected float[] getlineDivisions(int start_position,double length,int intervals)
-	{
-		float[] positions=new float[intervals-1];
-		double increment=length/intervals;
-		for(int i=0;i<intervals-1;i++)
-		{
-			positions[i]=start_position+(float)(increment*(i+1));
-		}
-		return positions;
-		
-	}
+
 	public static void main(String[] args)
 	{
 		JFrame.setDefaultLookAndFeelDecorated(true);
@@ -142,7 +130,7 @@ public class XY_GraphPane extends JPanel implements Graph
 		JFrame frame =new JFrame("Testing XY_Graph");
 		frame.setSize(500,500);
 		frame.setLayout(new BorderLayout());
-		frame.setVisible(true);
+		
 		frame.addWindowListener(new WindowAdapter()
 		{
 
@@ -156,14 +144,16 @@ public class XY_GraphPane extends JPanel implements Graph
 		});
 		Double[] x={10.0,20.0,30.0,40.0,50.0};
 		double[] y={10,30,50,10,40};
-		XY_GraphPane xy=new XY_GraphPane(100,100,10,10);
+		XY_GraphPane xy=new XY_GraphPane(60,60,6,6);
+		AxisDecorator axises=xy.new AxisDecorator(xy);
 		xy.setTitle("My Graph");
 		xy.setX_axis("X axis");
 		xy.setY_axis("Y axis");
 		xy.setX_values(x);
 		xy.setY_values(y);
-		frame.add(xy,BorderLayout.CENTER);
-		
+		//frame.add(axises,BorderLayout.CENTER);
+		frame.add(axises,BorderLayout.CENTER);
+		frame.setVisible(true);
 	}
 	class MyCanvas extends JPanel
 	{
@@ -175,6 +165,7 @@ public class XY_GraphPane extends JPanel implements Graph
 			super();
 			this.setOpaque(true);
 			this.setBackground(Color.WHITE);
+			this.repaint();
 		}
 
 		/**
@@ -185,43 +176,20 @@ public class XY_GraphPane extends JPanel implements Graph
 		protected void paintComponent(Graphics g)
 		{
 			super.paintComponent(g);
+			Graphics2D g2d=(Graphics2D)g;
 			double height=this.getSize().getHeight();
 			double width=this.getSize().getWidth();
-			Graphics2D g2d=(Graphics2D)g;
-			a =new Point2D.Float(50,00);
-			b =new Point2D.Float(50,(float)height-50);
-			c =new Point2D.Float((float)width,(float)height-50);
-			Line2D.Float y_line=new Line2D.Float(a,b);
-			Line2D.Float x_line=new Line2D.Float(b,c);
-			g2d.draw(y_line);
-			g2d.draw(x_line);
 			
-			float[] y_divisions=getlineDivisions(0,Utils.getLength(y_line),y_interval);
-			for(int i=0;i<y_divisions.length;i++)
-			{
-				Line2D.Float line=new Line2D.Float((float)a.getX()-10,y_divisions[i],(float)a.getX()+10,y_divisions[i]);
-				g2d.draw(line);
-			}
-			float[] x_divisions=getlineDivisions((int)b.getX(),Utils.getLength(x_line),x_interval);
-			for(int i=0;i<x_divisions.length;i++)
-			{
-				Line2D.Float line=new Line2D.Float(x_divisions[i],(float)b.getY()-10,x_divisions[i],(float)b.getY()+10);
-				g2d.draw(line);
-			}
 			double x_ratio=width/x_range;
 			double y_ratio=height/y_range;
 			Double[] x_values=(Double[])graph.getX_values();
 			double[] y_values=graph.getY_values();
-			AffineTransform transform2=AffineTransform.getTranslateInstance(0,height);
-			//g2d.transform(transform2);
-			AffineTransform transform1=AffineTransform.getTranslateInstance(b.getX(),b.getY());
-			g2d.transform(transform1);
-			
-			Point2D.Double temp=new Point2D.Double(0.0,0.0);
+
+			Point2D.Double temp=new Point2D.Double(0.0,height);
 			for(int i=0;i<graph.noOfDataSets();i++)
 			{
-				Point2D.Double point=new Point2D.Double(x_values[i]*x_ratio,y_values[i]*y_ratio);
-				point=Utils.xaxis_flip(point);
+				Point2D.Double point=new Point2D.Double(x_values[i]*x_ratio,height-y_values[i]*y_ratio);
+			//	System.out.println("Datapoint=("+point.getX()+","+point.getY()+")");
 				Rectangle2D.Double rec=new Rectangle2D.Double(point.getX()-5,point.getY()-5,10,10);
 				g2d.draw(rec);
 				Point2D.Double endPoint=new Point2D.Double(point.getX(),point.getY());
@@ -232,6 +200,32 @@ public class XY_GraphPane extends JPanel implements Graph
 			g2d.dispose();
 		}
 	}
-
+	public class AxisDecorator extends JPanel
+	{
+		JPanel instance=null;
+		private XAxis_Numbered x_axis;
+		private YAxis_Numbered y_axis;
+		public  AxisDecorator(JPanel panel)
+		{
+			this.instance=panel;
+			JPanel south=new JPanel(new BorderLayout());
+			this.setLayout(new BorderLayout());
+			
+			y_axis=new YAxis_Numbered(y_range,y_interval);
+			
+			y_axis.setPreferredSize(new Dimension(50,50));
+			x_axis=new XAxis_Numbered(x_range,x_interval);
+			x_axis.setPreferredSize(new Dimension(100,50));
+			this.add(instance,BorderLayout.CENTER);
+			this.add(y_axis,BorderLayout.WEST);
+			JLabel filler =new JLabel();
+			filler.setPreferredSize(new Dimension(50,50));
+			south.add(x_axis,BorderLayout.CENTER);
+			south.add(filler,BorderLayout.WEST);
+			this.add(south,BorderLayout.SOUTH);
+			
+		}
+		
+	}
 }
 
