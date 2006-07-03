b9a2c6ba710d8020d45b4937927dfbcba7489cc4
adrian <adrian@651d49b6-7517-0410-9a13-a4ecc4006b63>
2006-07-03 09:25:53 +0000

    A badly implement sort algorithm
    git-svn-id: file:///home/adrian/Documents/SVNRepository/Java/AMC@17 651d49b6-7517-0410-9a13-a4ecc4006b63

---
+++ trunk/AMC/src/org/amc/util/ASortAlgorithm.java
@@ -0,0 +1,71 @@
+package org.amc.util;
+
+import javax.swing.text.Position;
+
+/**
+ * <p>Project: AMC </p>
+ * <p>file: $URL$<p>
+ * <p>Created on Jul 3, 2006</p>
+ * @author adrian
+ * @version $Revision$
+ */
+
+public class ASortAlgorithm
+{
+	double[] array;
+	int pointer;
+	public ASortAlgorithm(double[] array)
+	{
+		this.array=array;
+	}
+	public void sort()
+	{
+		pointer=0;
+		
+		while(pointer!=array.length)
+		{
+			int saved=pointer;
+			for(int i=pointer+1;i<array.length;i++)
+			{
+				if(array[i]<array[pointer])
+				{
+					if(array[i]<array[saved])
+					{
+						saved=i;
+					}
+				}
+			}
+			if(saved!=-1)
+			{
+				double temp=array[saved];
+				array[saved]=array[pointer];
+				//System.out.println("Swapped "+array[pointer]+" and "+temp);
+				array[pointer]=temp;
+				
+			}
+			//System.out.println(toString(array));
+			pointer++;
+		}
+	}
+	public double[] getArray()
+	{
+		return array;
+	}
+	public String toString(double[] list)
+	{
+		String output="";
+		for(double value:list)
+		{
+			output=output+value+" ";
+		}
+		return output;
+	}
+	public static void main(String[] args)
+	{
+		double[] list={2,6,3,7,1,1,3,5,6,7,4,3,3,4,22,33,4,0};
+		ASortAlgorithm alg=new ASortAlgorithm(list);
+		alg.sort();
+
+	}
+
+}
