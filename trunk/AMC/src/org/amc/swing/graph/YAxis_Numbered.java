3b6ff220be52841852f1d51e7b5226b66ef26c6f
adrian <adrian@651d49b6-7517-0410-9a13-a4ecc4006b63>
2006-07-03 09:28:27 +0000

    This class was removed from XY_Graph_Pane and modified to be more generic.
    git-svn-id: file:///home/adrian/Documents/SVNRepository/Java/AMC@22 651d49b6-7517-0410-9a13-a4ecc4006b63

---
+++ trunk/AMC/src/org/amc/swing/graph/YAxis_Numbered.java
@@ -0,0 +1,50 @@
+package org.amc.swing.graph;
+import java.awt.*;
+import java.awt.geom.*;
+import javax.swing.*;
+/**
+ * <p>Project: AMC </p>
+ * <p>file: $URL$<p>
+ * <p>Created on Jul 2, 2006</p>
+ * @author adrian
+ * @version $Revision$
+ */
+
+public class YAxis_Numbered extends JLabel
+{
+
+	private int intervals;
+	private double range;
+	public YAxis_Numbered(double range,int intervals)
+	{
+		super();
+		this.intervals=intervals;
+		this.range=range;
+	}
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		Graphics2D g2d=(Graphics2D)g;
+		double height=this.getSize().getHeight();
+		double width=this.getSize().getWidth();
+		Point2D.Float a =new Point2D.Float((float)width-1,00);
+		Point2D.Float b =new Point2D.Float((float)width-1,(float)height);
+		Line2D.Float y_line=new Line2D.Float(a,b);
+		g2d.draw(y_line);
+		float[] y_divisions=AbstractGraph.getlineDivisions(0,height,intervals);
+		float increment=(float)range/intervals;
+		float index=(float)range-increment;
+		for(int i=0;i<y_divisions.length;i++)
+		{
+			
+			Line2D.Float line=new Line2D.Float((float)a.getX()-10,y_divisions[i],(float)a.getX(),y_divisions[i]);
+			g2d.draw(line);
+			g.drawString(String.valueOf(index),0,(int)y_divisions[i]);
+			index=index-increment;
+		}
+	}
+	
+}
+
+
